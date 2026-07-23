39449453692ffb9fed8cd0ff9ebe1d9d152a8569
Steve Gamarra da Silva <stevegamarra@yahoo.com.br>
2026-07-23 16:36:14 -0300

    Adiciona classe Recurso

---
+++ app/models.py
@@ -0,0 +1,7 @@
+class Recurso:
+
+    def __init__(self, titulo, descricao):
+
+        self.titulo = titulo
+
+        self.descricao = descricao
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app.models import Recurso
 
 
 @app.route("/")
@@ -7,31 +8,32 @@ def home():
 
     recursos = [
 
-        recurso("chamados", "gerencie solicitaçõs."),
-        recurso("inventario", "controle computadores")
+        Recurso(
+            "Chamados",
+            "Gerencie solicitações técnicas."
+        ),
+
+        Recurso(
+            "Inventário",
+            "Controle computadores, notebooks e ativos."
+        ),
+
+        Recurso(
+            "Usuários",
+            "Cadastre colaboradores e acompanhe acessos."
+        ),
+
+        Recurso(
+            "Relatórios",
+            "Visualize indicadores e desempenho da equipe."
+        ),
+
+        Recurso(
+            "Financeiro",
+            "Controle receitas, despesas e fluxo de caixa."
+        )
 
     ]
-    {
-            "titulo": "Inventário",
-            "descricao": "Controle computadores, notebooks e ativos."
-        },
-
-    {
-            "titulo": "Usuários",
-            "descricao": "Cadastre colaboradores e acompanhe acessos."
-        },
-
-    {
-            "titulo": "Relatórios",
-            "descricao": "Visualize indicadores e desempenho da equipe."
-        },
-
-    {
-            "titulo": "Financeiro",
-            "descricao": "Controle receitas, despesas e fluxo de caixa."
-        }
-
-    
 
     return render_template(
         "index.html",
