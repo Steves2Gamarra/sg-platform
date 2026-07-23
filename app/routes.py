05e4c16b69aac460b4ced5388ceb3faeab5cbd9a
Steve Gamarra da Silva <stevegamarra@yahoo.com.br>
2026-07-23 15:48:42 -0300

    Minha mensagem explicativa

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,39 @@
 from flask import render_template
-
 from app import app
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+
+    recursos = [
+
+        recurso("chamados", "gerencie solicitaçõs."),
+        recurso("inventario", "controle computadores")
+
+    ]
+    {
+            "titulo": "Inventário",
+            "descricao": "Controle computadores, notebooks e ativos."
+        },
+
+    {
+            "titulo": "Usuários",
+            "descricao": "Cadastre colaboradores e acompanhe acessos."
+        },
+
+    {
+            "titulo": "Relatórios",
+            "descricao": "Visualize indicadores e desempenho da equipe."
+        },
+
+    {
+            "titulo": "Financeiro",
+            "descricao": "Controle receitas, despesas e fluxo de caixa."
+        }
+
+    
+
+    return render_template(
+        "index.html",
+        recursos=recursos
+    )
